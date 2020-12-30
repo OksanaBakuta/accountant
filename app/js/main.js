267913e3c6a94c3c5ef7e146ea6bc63b4544c673
OksanaBakuta <oksanabakuta@gmail.com>
2020-12-31 00:35:04 +0200

    added section partners and review

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
   
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
 });
